58dd45a00b544e294072953cd25d70cc70d6540b
Garcia200876 <davizinhogarcia@email.com>
2026-06-17 20:16:27 -0300

    feat: Criando a controller para receber a api

--- APIProjeto/Controllers/HomeController.cs
+++ APIProjeto/Controllers/HomeController.cs
@@ -1,20 +1,24 @@
+using System.Diagnostics;
 using APIProjeto.Models;
 using Microsoft.AspNetCore.Mvc;
-using System.Diagnostics;
 
 namespace APIProjeto.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IHttpClientFactory _httpClientFactory;
+        public HomeController(ILogger<HomeController> logger, IHttpClientFactory httpClientFactory)
+        {
+            _logger = logger;
+            _httpClientFactory = httpClientFactory;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
---
+++ APIProjeto/Models/Postagem.cs
@@ -0,0 +1,9 @@
+﻿namespace APIProjeto.Models
+{
+    public class Postagem
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? Body { get; set; }
+    }
+}
